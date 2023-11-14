db25fadc2c5815862abeab52c1f48d122158c372
Nakanishi <45790603+Nakanishi123@users.noreply.github.com>
2023-11-14 17:28:45 +0900

    整形

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  site: "https://Nakanishi123.github.io",
+  base: "/NonsenseMemo",
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '🚮メモ置き場';
-export const SITE_DESCRIPTION = 'あ';
-export const AUTHOR="Nakanishi123"
+export const SITE_TITLE = "🚮メモ置き場";
+export const SITE_DESCRIPTION = "あ";
+export const AUTHOR = "Nakanishi123";
+export const URL_BASE = "/NonsenseMemo";
