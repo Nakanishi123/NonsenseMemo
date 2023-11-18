4d4349dc1f45ac043c0fd1346acd390bc26316fc
Nakanishi <45790603+Nakanishi123@users.noreply.github.com>
2023-11-18 19:36:03 +0900

    整形

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
