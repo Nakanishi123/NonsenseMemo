428a7f62967254c558616f142e819c129b58af8b
Nakanishi <45790603+Nakanishi123@users.noreply.github.com>
2023-11-21 23:24:48 +0900

    blog -> memo

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { SITE_DESCRIPTION, SITE_TITLE, URL_BASE } from "../consts";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("memo");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map(post => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `${URL_BASE}/memo/${post.slug}/`,
     })),
   });
 }
