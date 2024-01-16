351cb1e6ed1a024f9a0d2efcc3d1bd113888089f
Nakanishi <45790603+Nakanishi123@users.noreply.github.com>
2024-01-16 11:12:11 +0900

    検索変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Nakanishi123.github.io",
   base: "/NonsenseMemo",
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [mdx(), sitemap(), react(), tailwind(), pagefind()],
 });
