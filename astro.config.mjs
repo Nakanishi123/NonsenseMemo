aab3ab145906ba0e583b9e4459f1302a6ed2c41a
M.Nakanishi <contact114514@nakanishi.dev>
2024-06-19 22:52:07 +0900

    astro-iconの導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import pagefind from "astro-pagefind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Nakanishi123.github.io",
   base: "/NonsenseMemo",
-  integrations: [mdx(), sitemap(), react(), tailwind(), pagefind()],
+  integrations: [mdx(), sitemap(), react(), tailwind(), pagefind(),icon()],
 });
