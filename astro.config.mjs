ffd5dfe481430d6d376245e0ac5cd901739c19e3
M.Nakanishi <contact114514@nakanishi.dev>
2024-06-28 09:53:40 +0900

    markdowのtex対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,15 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nonsensememo.pages.dev",
   integrations: [mdx(), sitemap(), react(), tailwind(), pagefind(), icon()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
