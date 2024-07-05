09df0d33bd98d104b0f70bf15b4d676efddbe316
M.Nakanishi <contact114514@nakanishi.dev>
2024-07-06 00:00:21 +0900

    astro-rehype-img2picの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,14 @@ import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
+import rehypeImg2pic from "astro-rehype-img2pic";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nonsensememo.pages.dev",
-  integrations: [mdx(), sitemap(), react(), tailwind(), pagefind(), icon()],
+  integrations: [mdx(), sitemap(), react(), tailwind(), pagefind(), icon(), rehypeImg2pic()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
+    rehypePlugins: [rehypeKatex]
+  }
 });
